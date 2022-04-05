84d42e89a32cf2c1632ccc15ab476a2a641ff460
divyanshisuri <89176700+divyanshisuri@users.noreply.github.com>
2022-04-04 22:56:18 -0700

    Create bunny.py

---
+++ week0/bunny.py
@@ -0,0 +1,268 @@
+#bunny animation
+#prefuncy.py
+import time
+import os
+
+
+
+Color34 = "\u001b[34m"
+Color37 = "\u001b[37m"
+
+# As you can see, its not very optimal
+def bunny1():
+   print(" \_/ ")
+   print("(o.o)")
+   print(" (___)0  ")
+   print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+def bunny2():
+   print("   \_/ ")
+   print("  (o.o)")
+   print("   (___)0  ")
+   print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+
+
+
+
+def bunny3():
+   print("     \_/ ")
+   print("    (o.o)")
+   print("     (___)0  ")
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+def bunny4():
+   print("       \_/ ")
+   print("      (o.o)")
+   print("       (___)0  ")
+
+
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+def bunny5():
+   print("        \_/ ")
+   print("       (o.o)")
+   print("        (___)0  ")
+
+
+print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+def bunny6():
+   print("         \_/ ")
+   print("        (o.o)")
+   print("          (___)0  ")
+   print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+def bunny7():
+   print("       \_/ ")
+   print("       (o.o)")
+   print("        (___)0  ")
+
+
+
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+def bunny8():
+   print("        \_/ ")
+   print("       (o.o)")
+   print("          (___)0  ")
+   print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def bunny9():
+     print("          \_/ ")
+   print("           (o.o)")
+   print("            (___)0  ")
+   print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+def bunny10():
+    print("              \_/ ")
+   print("              (o.o)")
+   print("                (___)0  ")
+
+
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+def bunny11():
+   print("       \_/ ")
+   print("    (o.o)")
+   print("      (___)0  ")
+
+
+print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+
+
+
+
+def bunny12():
+   print("   \_/ ")
+   print("         (o.o)")
+   print("     (___)0  ")
+
+
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+
+
+
+
+def bunny13():
+   print("           \_/ ")
+   print("    (o.o)")
+   print("           (___)0  ")
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+
+
+
+
+def bunny14():
+   print("            \_/ ")
+   print("    (o.o)")
+   print("          (___)0  ")
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+
+
+
+
+def bunny15():
+     print("     \_/ ")
+   print("              (o.o)")
+   print("     (___)0  ")
+
+
+
+print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+
+
+
+
+def bunny16():
+    print("             \_/ ")
+   print("         (o.o)")
+   print("  (___)0  ")
+print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+
+
+
+
+def bunny17():
+   print("            \_/ ")
+   print("    (o.o)")
+   print("          (___)0  ")
+   print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+
+
+
+
+def bunny18():
+   print("                 \_/ ")
+   print("           (o.o)")
+   print("     (___)0  ")
+   print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+
+
+
+
+def bunny19():
+   print("              \_/ ")
+   print("          (o.o)")
+   print("     (___)0  ")
+   print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+
+
+
+
+def bunny20():
+   print("              \_/ ")
+   print("    (o.o)")
+   print("          (___)0  ")
+   print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+
+
+
+
+os.system("cls")
+time.sleep(.1)
+ship1()
+time.sleep(.5)
+os.system("cls")
+ship2()
+time.sleep(.5)
+os.system("cls")
+ship3()
+time.sleep(.5)
+os.system("cls")
+ship4()
+time.sleep(.5)
+os.system("cls")
+ship5()
+time.sleep(.5)
+os.system("cls")
+ship6()
+time.sleep(.5)
+os.system("cls")
+ship7()
+time.sleep(.5)
+os.system("cls")
+ship8()
+time.sleep(.5)
+os.system("cls")
+ship9()
+time.sleep(.5)
+os.system("cls")
+ship10()
+time.sleep(.5)
+os.system("cls")
+ship11()
+time.sleep(.5)
+os.system("cls")
+ship12()
+time.sleep(.5)
+os.system("cls")
+ship13()
+time.sleep(.5)
+os.system("cls")
+ship14()
+time.sleep(.5)
+os.system("cls")
+ship15()
+time.sleep(.5)
+os.system("cls")
+ship16()
+time.sleep(.5)
+os.system("cls")
+ship17()
+time.sleep(.5)
+os.system("cls")
+ship18()
+time.sleep(.5)
+os.system("cls")
+ship19()
+time.sleep(.5)
+os.system("cls")
+ship20()
+time.sleep(.5)
+os.system("cls")
+print("or so you thought...")
+time.sleep(.5)
+os.system("cls")
